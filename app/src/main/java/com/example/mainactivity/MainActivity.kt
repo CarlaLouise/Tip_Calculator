0c2127aa37f43c9c3c8ede2263c5187bafe733e6
CarlaLouise <148821309+CarlaLouise@users.noreply.github.com>
2024-06-20 23:29:54 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mainactivity/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mainactivity
+
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mainactivity.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    // Valor total conta
+    // Numero de pessoas
+    // Porcentagem da gorjeta
+    // 10%, 15% ou 20%
+    // Calcular
+    // Limpar
+
+    // Recuperar as Views do layout
+    // ViewBinding
+
+    private lateinit var binding: ActivityMainBinding
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnClean.setOnClickListener {
+                println("Roque1" + binding.tieTotal.text)
+                println("Roque1" + binding.tieNumPeople.text)
+        }
+
+    }
+}
