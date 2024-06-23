7387048483de18f143de9b366dd777ab590f5de5
CarlaLouise <148821309+CarlaLouise@users.noreply.github.com>
2024-06-22 23:12:02 -0300

    Implementado o resultado do calculo

--- app/src/main/java/com/example/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/mainactivity/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.mainactivity
 
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mainactivity.databinding.ActivityMainBinding
-import com.google.android.material.textfield.TextInputEditText
+
 
 class MainActivity : AppCompatActivity() {
+
 
     // Valor total conta
     // Numero de pessoas
@@ -20,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     // Recuperar as Views do layout
     // ViewBinding
 
+    // Recuperar os radio buttons
+
     private lateinit var binding: ActivityMainBinding
 
         override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +27,44 @@ class MainActivity : AppCompatActivity() {
             binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        binding.btnClean.setOnClickListener {
-
+        var percentage:Int = 0
+        binding.rbOptionOne.setOnCheckedChangeListener {_, isChecked ->
+            println("Roque1 Option one:$isChecked")
+            if(isChecked){
+                percentage = 10
+            }
         }
 
+
+        binding.rbOptionTwo.setOnCheckedChangeListener {_, isChecked ->
+            if(isChecked){
+                percentage = 15
+            }
+        }
+
+        binding.rbOptionThree.setOnCheckedChangeListener {_, isChecked ->
+            if(isChecked){
+                percentage = 20
+            }
+        }
+
+
+
+        binding.btnClean.setOnClickListener {
+                println("Roque1 " + binding.tieTotal.text)
+                println("Roque1 " + binding.tieNumPeople.text)
+        }
+
+
+        binding.btnDone.setOnClickListener {
+            val totalTable: Float = binding.tieTotal.text.toString().toFloat()
+            val nPeople: Int = binding.tieNumPeople.text.toString().toInt()
+
+            val totalTemp = totalTable / nPeople
+            val tips = totalTemp * percentage / 100
+            val totalWithTips = totalTemp + tips
+            println("Roque1 " + totalWithTips)
+
+        }
     }
 }
