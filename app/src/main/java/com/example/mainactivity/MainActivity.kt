6ce373d59564a917969e6bdaa121097543c521af
CarlaLouise <148821309+CarlaLouise@users.noreply.github.com>
2024-06-28 01:32:23 -0300

    Levando valores para outra tela

--- app/src/main/java/com/example/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/mainactivity/MainActivity.kt
@@ -95,19 +95,24 @@ class MainActivity : AppCompatActivity() {
                 val totalTemp = totalTable / nPeople
                 val tips = totalTemp * percentage / 100
                 val totalWithTips = totalTemp + tips
-                binding.tvResult.text = "Total with tips: $totalWithTips"
 
+                val intent = Intent (this, SummaryActivity::class.java)
+                intent.apply {
+                    putExtra("totalTable", totalTable)
+                    putExtra("numPeople", numOfPeopleSelected)
+                    putExtra("percentage", percentage)
+                    putExtra("totalAmount", totalWithTips)
+                }
+                startActivity(intent)
             }
         }
             binding.btnClean.setOnClickListener {
-                binding.tvResult.text = ""
                 binding.tieTotal.setText("")
                 binding.rbOptionOne.isChecked = false
                 binding.rbOptionTwo.isChecked = false
                 binding.rbOptionThree.isChecked = false
 
-                val intent = Intent (this, SummaryActivity::class.java)
-                startActivity(intent)
+
 
         }
     }
